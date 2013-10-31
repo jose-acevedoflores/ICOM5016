b9966bc46c923ba46587b85414cc824b0d78d9a5
andres.malines@upr.edu <andres.malines@upr.edu>
2013-10-31 01:01:55 -0400

    implemented login as a query to the DB

--- ExperimentalJose/Ex1/server.js
+++ ExperimentalJose/Ex1/server.js
@@ -331,47 +331,45 @@ app.get("/search/:query", function(req, res)  {
 });
 
 
-// REST Operation - HTTP POST to login
 app.put("/userLogin", function(req, res){
-	
-	console.log("POST  : Login");
-	console.log(req.body.hasOwnProperty('emailAddress'));
-	
-	var email = req.body.emailAddress;
-	var password = req.body.userPassword;
-	console.log(email);
-	
-	var target = -1;
-	for(var i =0; i < userList.length; i++) {
-		var userEmail = userList[i].emailAddress;
-		var userPassword = userList[i].password;
-		console.log(i);
-		console.log(userEmail);
-		console.log(email);
-		console.log(userPassword);
+        
+    console.log("POST  : Login");
+    console.log(req.body.hasOwnProperty('emailAddress'));
+    var password = req.body.password;
+    var email = req.body.emailAddress;
+        
+    console.log(email);
+    console.log(password);
+  	var client = new pg.Client(conString);
+ 	client.connect();
+  
 
-		if (userEmail === email ){
-			target = i;
-			break;
-		}
-	}
-	if(target==-1){
-		res.statusCode = 401;
-		res.send("There was an error with your e-mail/password combination.");
+    var query = client.query("SELECT email_address, password from web_user where (web_user.email_address = $1 AND web_user.password = $2)", [email, password]);
+    
+    query.on("row", function (row, result) {
+    	result.addRow(row);
 
-	}
-	else {
-		if(userList[i].password === userPassword){
-			loggedIn = true;
-			res.json(true);
-		}
-		else {
-			res.statusCode = 401;
-			res.send("There was an error with your e-mail/password combination.");
-		}
-	}
-	
+    });
+
+    query.on("end", function (result) {
+    	var len = result.rows.length;
+    	if (len==0) {
+    		res.statusCode = 401;
+    		res.send("There was an error with your e-mail/password combination.");
+     	}
+
+      	else {
+
+        	loggedIn = true;
+        	res.json(true);
+
+     	 }
+      	
+      	client.end();
+
+    });
 });
+
 
 // REST Operation - HTTP PUT to update a bid
 app.put("/placedBids/item:id", function(req, res){
